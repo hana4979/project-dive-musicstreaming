107f94fe73f5d9d915d34d9b013f83866d2a4a0c
hana <o3o_s2__@naver.com>
2025-03-10 16:14:22 +0900

    Add test code

--- src/main/java/com/himedia/projectteamdive/security/filter/JWTCheckFilter.java
+++ src/main/java/com/himedia/projectteamdive/security/filter/JWTCheckFilter.java
@@ -75,6 +75,10 @@ public class JWTCheckFilter extends OncePerRequestFilter {
             return true;
         }
 
+        if(path.startsWith("/api/member/test")){
+            return true;
+        }
+
         if(path.startsWith("/api/member/login")){
             return true;
         }
